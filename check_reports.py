85e7714c5dedfd0c582c92c4c9d038c7a0086738
david1707 <DavidAlacant@gmail.com>
2019-09-15 12:54:58 +0200

    Now removes folders and its content

--- check_reports.py
+++ check_reports.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import sys
 
 
 passed_reports_folder = 'students_passed'
@@ -26,8 +27,25 @@ def process_reports():
 			move_to_folder(report, total_failed_subjects)
 
 	# Zip both folders
-	shutil.make_archive(failed_reports_folder, compression_type, failed_reports_folder)
-	shutil.make_archive(passed_reports_folder, compression_type, passed_reports_folder)
+	shutil.make_archive(failed_reports_folder,
+						compression_type, failed_reports_folder)
+	shutil.make_archive(passed_reports_folder,
+						compression_type, passed_reports_folder)
+
+
+def clean_everything():
+	# Clean txt and zip files
+	filename_list = os.listdir(os.getcwd())
+	for report in filename_list:
+		if report.endswith('_marks.txt') or report.endswith('.zip'):
+			os.remove(report)
+
+	# Clean folders
+	if os.path.exists(passed_reports_folder):
+		shutil.rmtree(passed_reports_folder)
+
+	if os.path.exists(failed_reports_folder):
+		shutil.rmtree(failed_reports_folder)
 
 
 def generate_folders():
@@ -52,11 +70,25 @@ def calculate_failed_subjects(subjects_and_marks):
 def move_to_folder(old_filename, total_failed_subjects):
 	if total_failed_subjects >= 2:
 		new_filename = '_'.join(old_filename.split('_')[:3]) + '_failed.txt'
-		os.rename(old_filename, os.path.join(failed_reports_folder, new_filename))
+		os.rename(old_filename, os.path.join(
+			failed_reports_folder, new_filename))
 
-	else: 
+	else:
 		new_filename = '_'.join(old_filename.split('_')[:3]) + '_passed.txt'
-		os.rename(old_filename, os.path.join(passed_reports_folder, new_filename))
+		os.rename(old_filename, os.path.join(
+			passed_reports_folder, new_filename))
+
 
 if __name__ == '__main__':
-	process_reports()
+	try:
+		action = sys.argv[1]
+	except IndexError:
+		pass
+
+	action = sys.argv[1]
+	if action == '-p':
+		process_reports()
+	elif action == '-d':
+		clean_everything()
+	else:
+		print('Action not recognized.')
