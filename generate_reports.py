59e6deb59c38542040c98060b3e9b612a0cac494
david1707 <DavidAlacant@gmail.com>
2019-09-15 12:32:55 +0200

    Added comments + tweaked the random grade generator. They were too stupid to pass

--- generate_reports.py
+++ generate_reports.py
@@ -14,20 +14,24 @@ def generate_reports(num):
     assert type(num) == int, 'Please, introduce a number to generate reports.'
 
     for i, student_number in enumerate(range(num)):
+        # Generate  values
         max_number_characters = len(str(num))
         student_name = names.get_full_name().lower().replace(' ', '_')
-
         filename = f'{str(i+1).zfill(max_number_characters)}_{student_name}_marks.txt'
 
         with open(filename, 'w') as f:
+
+            # Randomize and pick between 8 and 10 subjects
             random.shuffle(subjects)
             subjects_number = random.randint(8, 10)
             student_subjects = subjects[:subjects_number]
             report_generation_date = date.today() - timedelta(days=random.randint(0, 7))
 
+            # Generate a dictionary and then a string with the student subjects and their grade
             subjects_and_marks = generate_subjects_and_marks(student_subjects)
             student_marks_report = generate_marks_report(subjects_and_marks)
 
+            # Create a .txt report
             report_text = f'Student name: {student_name}\n\n' + \
                 f'Report generated on {report_generation_date}\n\n' + \
                 'Student final marks:\n\n' + \
@@ -43,7 +47,7 @@ def delete_reports():
 
 
 def generate_subjects_and_marks(student_subjects):
-    return {subject: round(random.uniform(3.5, 10), 1)
+    return {subject: round(random.uniform(4, 10), 1)
             for subject in student_subjects}
 
 
